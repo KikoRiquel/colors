53084d38d387022e5b4abba2b4f4a49691d0c088
kiko <kiko@kikos-MBP.docencia.educarm.net>
2023-03-01 12:14:43 +0100

    funcion copy implementada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+import colornames from '../assets/colornames.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private clipboard: Clipboard) {}
+
   title = 'colors';
+  public colorList: { name: string; hex: string }[] = colornames;
+
+  copyHexCode(hex: string) {
+    this.clipboard.copy(hex);
+  }
 }
